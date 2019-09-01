5d46858452a9447f2693951b0baccbf20da94254
RonWaller <ron.waller7@gmail.com>
2019-08-31 20:31:57 -0400

    Finished main layout, started javascript

---
+++ build/js/main.js
@@ -0,0 +1,65 @@
+const container = document.querySelector('.container');
+
+async function getMovies() {
+	let movies = [];
+	const api = '9460c3936e9e9f9d5fd2b7f0b50f733b';
+	const year = '2019';
+
+	for (i = 1; i < 4; i++) {
+		let url = `https://api.themoviedb.org/3/discover/movie?api_key=${api}b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${i}&primary_release_year=${year}`;
+		const response = await axios.get(url);
+		const { data } = response;
+		const results = data.results;
+		results.forEach(item => {
+			movies.push(item);
+		});
+	}
+	movies.splice(50, 10);
+	return movies;
+}
+
+getMovies()
+	.then(res => {
+		console.log('movies:', res);
+	})
+	.catch(err => console.log(err));
+
+function buildContent() {
+	const card = document.createElement('div');
+	const img = document.createElement('img');
+	const movieInfo = document.createElement('div');
+	const ul = document.createElement('ul');
+	const item = document.createElement('li');
+	const movieDescription = document.createElement('div');
+	const p = document.createElement('p');
+}
+
+// let div = document.createElement('div');
+// let img = document.createElement('img');
+// let footer = document.createElement('footer');
+// let unsplash_url =
+//   'https://unsplash.com/?utm_source=your_app_name&utm_medium=referral';
+// img.src = url;
+// img.id = `img-${index}`;
+// footer.id = 'footer';
+// footer.innerHTML = `Photo by: <br> <a href='${link}' target="_blank">${user}</a> on <a href='${unsplash_url}' target="_blank">Unsplash</a>`;
+// div.appendChild(img);
+// div.appendChild(footer);
+// modalContainer.appendChild(div);
+
+// <div class="card">
+// 	<div class="movie__info">
+//     <img src="./img/lion_king.jpg" alt="Lion King" />
+//     <ul>
+//       <li>July 19, 2019</li>
+//       <li><a href="#">More Info</a></li>
+//     </ul>
+//   </div>
+//   <div class="movie__description">
+//     <p>
+//       Simba idolises his father, King Mufasa, and takes to heart his own
+//       royal destiny. But not everyone in the kingdom celebrates the new
+//       cub's arrival. Scar, Mufasa's brother—and former heir to the
+//       throne—has plans…
+//   </p>
+// </div>
