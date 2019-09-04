3cf4c3dfcce922862eb0e14bc173008a287d28a1
RonWaller <ron.waller7@gmail.com>
2019-09-04 14:56:20 -0400

    Added movies by year functionality

--- build/js/main.js
+++ build/js/main.js
@@ -1,9 +1,10 @@
+const year = '2019';
 const container = document.querySelector('.container');
+const movieYear = document.querySelector('.movie_year');
 
-async function getMovies() {
+async function getMovies(year) {
 	let movies = [];
 	const api = '9460c3936e9e9f9d5fd2b7f0b50f733b';
-	const year = '2019';
 
 	for (i = 1; i < 4; i++) {
 		let url = `https://api.themoviedb.org/3/discover/movie?api_key=${api}b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page= ${i} &primary_release_year= ${year}`;
@@ -18,41 +19,52 @@ async function getMovies() {
 	return movies;
 }
 
-getMovies()
-	.then(movies => {
-		console.log('movies:', movies);
-		return buildContent(movies);
-	})
-	.catch(err => console.log(err));
-
 function buildContent(movies) {
-	movies.forEach(movie => {
-		image_baseurl = 'https://image.tmdb.org/t/p/';
-		poster_size = 'w154';
-		let date = moment(movie.release_date, 'YYYY/MM/DD').format('LL');
-		const card = document.createElement('div');
-		card.classList.add('card');
-		const movieInfo = document.createElement('div');
-		movieInfo.classList.add('movie__info');
-		const img = document.createElement('img');
-		img.src = `${image_baseurl}${poster_size}${movie.poster_path}`;
-		img.alt = `${movie.original_title}`;
-		const ul = document.createElement('ul');
-		const firstItem = document.createElement('li');
-		firstItem.innerHTML = `${date}`;
-		const secondItem = document.createElement('li');
-		secondItem.innerHTML = `<a href="#">More Info</a>`;
-		const movieDescription = document.createElement('div');
-		movieDescription.classList.add('movie__description');
-		const p = document.createElement('p');
-		p.innerHTML = `${movie.overview}`;
-		ul.appendChild(firstItem);
-		ul.appendChild(secondItem);
-		movieInfo.appendChild(img);
-		movieInfo.appendChild(ul);
-		movieDescription.appendChild(p);
-		card.appendChild(movieInfo);
-		card.appendChild(movieDescription);
-		container.appendChild(card);
-	});
+	image_baseurl = 'https://image.tmdb.org/t/p/';
+	poster_size = 'w154';
+	const html = movies
+		.map(movie => {
+			let date = moment(movie.release_date, 'YYYY/MM/DD').format('LL');
+			const htmlString = `
+				<div class="card">
+					<div class="movie__info">
+						<img src="${image_baseurl}${poster_size}${movie.poster_path}" alt="${movie.original_title}">
+						<ul>
+							<li>${date}</li>
+							<li><a href="#">More Info</a></li>
+						</ul>
+					</div>
+					<div class="movie__description">
+						<p>${movie.overview}</p>
+					</div>
+				</div>`;
+			return htmlString;
+		})
+		.join('');
+	container.innerHTML = html;
 }
+
+function init(year) {
+	getMovies(year)
+		.then(movies => {
+			return buildContent(movies);
+		})
+		.catch(err => console.log(err));
+}
+
+document.addEventListener('DOMContentLoaded', init(year));
+
+movieYear.addEventListener('click', e => {
+	const target = e.target;
+	if (target.tagName === 'LI') {
+		const activeElement = movieYear.querySelector('.active');
+		if (activeElement) {
+			activeElement.classList.remove('active');
+		}
+		if (activeElement !== target) {
+			target.classList.add('active');
+			const targetYear = target.innerHTML;
+			init(targetYear);
+		}
+	}
+});
