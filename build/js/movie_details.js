b3eabff263100856eef12572aa9f16a325c9d9a5
RonWaller <ron.waller7@gmail.com>
2019-09-06 21:57:05 -0400

    Formatting details page

--- build/js/movie_details.js
+++ build/js/movie_details.js
@@ -7,8 +7,6 @@ async function movieDetails(movieID) {
 	let url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api}&language=en-US&append_to_response=release_dates,credits`;
 	const response = await axios.get(url);
 	const { data } = response;
-	// const results = data.results;
-	console.log(data);
 	return data;
 }
 
@@ -16,17 +14,37 @@ function buildDetail(movie) {
 	image_baseurl = 'https://image.tmdb.org/t/p/';
 	poster_size = 'w342';
 	document.title = `${movie.title} | The Movie Island `;
+	movie_details.style.backgroundImage = `url('${image_baseurl}original${movie.backdrop_path}')`;
+	const credits = movie.credits.cast;
+	credits.splice(5, 43);
+	const actors_html = credits
+		.map(actor => {
+			const url = `${image_baseurl}w154${actor.profile_path}`;
+			const htmlString = `
+    <div class="actor">
+				<img src="${url}" alt="${actor.name}" />
+				<h4>${actor.name}</h4>
+				<h6>${actor.character}</h6>
+		</div>
+    `;
+			return htmlString;
+		})
+		.join('');
+	console.log(credits);
 	const html = `
     <div class="big_card">
+      <div class='goback'><-- Go Back</div>
       <div class="movie_poster">
         <img src="${image_baseurl}${poster_size}${movie.poster_path}" alt="">
       </div>
       <div class="detailed_info">
         <!-- movie information goes here -->
-        <h3>${movie.title}</h3>
+        <h2>${movie.title}</h2>
         <p>${movie.overview}</p>
       </div>
-      <div class='movie_actors'></div>
+      <div class='movie_actors'>
+        ${actors_html}
+      </div>
     </div>`;
 	movie_details.innerHTML = html;
 }
